17dfea475c611275442ce935b8ff99f2ac7cc305
shreyajha24253-lang <shreyajha24253@gmail.com>
2026-02-02 16:25:41 +0530

    Update StackUsingQueue.java

--- src/main/java/StackUsingQueue.java
+++ src/main/java/StackUsingQueue.java
@@ -10,19 +10,19 @@ public class StackUsingQueue<E> implements StackADT<E> {
     @Override
     public int size() {
         // TODO: return q.size()
-        return -1;
+        return q.size();
     }
 
     @Override
     public boolean isEmpty() {
         // TODO: return q.isEmpty()
-        return true;
+        return q.isEmpty();
     }
 
     @Override
     public E top() {
         // TODO: return q.first()
-        return null;
+        return q.first();
     }
 
     @Override
@@ -31,14 +31,18 @@ public class StackUsingQueue<E> implements StackADT<E> {
 
         // TODO:
         // 1) q.enqueue(e)
+        q.enqueue(e);
         // 2) rotate (size-1) times:
+        for (int= 0; i< q.size() - 1; i++) {
+            q.enqueue(q.dequeue());
+        }
+    } 
         //    q.enqueue(q.dequeue());
         // so the newest element becomes the front of the queue.
-    }
 
     @Override
     public E pop() {
         // TODO: return q.dequeue()
-        return null;
+        return q.dequeue();
     }
 }
